1af7abb30acb9185c2b553f7ac45e6cf94a608b2
Gabriel Masson <gmasson@users.noreply.github.com>
2015-11-03 20:20:55 -0200

    Delete connect.php

--- db_php/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-  include 'info.php';
-  try { $connect_mysql = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password); }
-  catch(PDOException $e) { echo $e->getMessage(); }
-?>
