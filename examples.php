000ad9e5f8e50be9477d36c6655be4711198d6fc
Gabriel Masson <gmasson@users.noreply.github.com>
2016-09-12 11:15:49 -0300

    Update examples.php

--- examples.php
+++ examples.php
@@ -5,24 +5,24 @@
 
   /* INSERT */
   $insert = $database_conexBD->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
-  $insert->bindParam(":name","Full Name"); 
-  $insert->bindParam(":email","email@email.com"); 
+  $insert->bindValue(":name", "Full Name"); 
+  $insert->bindValue(":email", "email@email.com"); 
   $insert->execute();
 
   /* DELETE */
   $delete = $database_conexBD->prepare("DELETE FROM users WHERE id=:id");
-  $delete->bindParam( ":id", "1", PDO::PARAM_STR );
+  $delete->bindValue( ":id", "1", PDO::PARAM_STR );
   $delete->execute();
 
   /* UPDATE */
   $update = $database_conexBD->prepare("UPDATE users SET name=:name WHERE id=:id");
-  $update->bindParam(":name","Full Name"); 
-  $update->bindParam(":id","1"); 
+  $update->bindValue(":name", "Full Name"); 
+  $update->bindValue(":id", "1"); 
   $update->execute();
 
   /* SELECT */
   $select = $database_conexBD->prepare("SELECT * FROM users WHERE name LIKE :search");
-  $select->bindParam( ":search", "%test%" );
+  $select->bindValue( ":search", "%test%" );
   $select->execute();
 
 ?>
