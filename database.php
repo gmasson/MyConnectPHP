3880c513823dd17be41c10bfff2afe6f3421011d
Gabriel Masson <gmasson@users.noreply.github.com>
2022-03-23 01:02:30 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,16 @@
 <?php
-  session_start();
+/**
+ * PDOConn - Database 
+*/
 
-  // Database
-  $database_type = 'mysql'; // Serv MySQL 
-  $database_serv = 'localhost:3306'; // Serv MySQL 
-  $database_user = 'root'; // User MySQL
-  $database_pass = 'root'; // Password MySQL
-  $database_bd = 'name_database'; // Database
+// Database
+$database_type = 'mysql'; // Driver
+$database_serv = 'localhost:3306'; // Serv MySQL 
+$database_user = 'root'; // User MySQL
+$database_pass = 'root'; // Password MySQL
+$database_bd = 'database'; // Database
 
-  // Connection
-  $database_conexBD = new PDO("{$database_type}:host={$database_serv};dbname={$database_bd}", "{$database_user}", "{$database_pass}");
-  $database_conexBD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $database_conexBD->exec("set names utf8");
+// Connection
+$database_conexBD = new PDO("{$database_type}:host={$database_serv};dbname={$database_bd}", "{$database_user}", "{$database_pass}");
+$database_conexBD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$database_conexBD->exec("set names utf8");
